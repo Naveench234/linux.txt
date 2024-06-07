b050cbc920ee58f02b19e18461263c76c10dde21
Naveench234 <160734247+Naveench234@users.noreply.github.com>
2024-06-07 12:13:29 +0530

    from main

--- python.py
+++ python.py
@@ -1,1 +1,12 @@
 welcome to python
+git work  commands
+git init
+git clone(url)
+cd floder path copy 
+working area
+ls
+vi file name 
+git add .
+git commit -m "msg"
+git log (to show the repo id )
+git push 9to show  in the loacl repo
